c9a2958867bf21f713dd45b933a83313db14634f
Misha Koroliovas <koroliov1985@gmail.com>
2025-04-08 01:20:31 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from 'next';
-//require('dotenv').config({ path: '.env' });
+require('dotenv').config({ path: '.env.' });
 
 const nextConfig: NextConfig = {
   /* config options here */
